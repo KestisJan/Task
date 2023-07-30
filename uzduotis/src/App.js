94939d407ee8fda5f411c0ae3c7cea443722c80b
Kestas <kestutisjankevicius9@gmail.com>
2023-07-30 20:17:45 +0300

    Layout

---
+++ uzduotis/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='container'>
+        <div id="item-0">Test</div>
+        <div id="item-1">&nbsp;</div>
+        <div id="item-2">&nbsp;</div>
+        <div id="item-3">&nbsp;</div>
+        <div id="item-4">&nbsp;</div>
+        <div id="item-5">&nbsp;</div>
+    </div>
+  );
+}
+
+export default App;
