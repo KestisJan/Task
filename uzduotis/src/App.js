9bb5b811d01cd5580b1c43a5b0ceecdd9b8c681e
Kestas <kestutisjankevicius9@gmail.com>
2023-07-30 20:51:45 +0300

    Footer

--- uzduotis/src/App.js
+++ uzduotis/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Navigation from './Components/Navigation';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className='container'>
         <div id="item-0"><Navigation/></div>
-        <div id="item-1">&nbsp;</div>
+        <div id="item-1"><Footer/></div>
         <div id="item-2">&nbsp;</div>
         <div id="item-3">&nbsp;</div>
         <div id="item-4">&nbsp;</div>
--- uzduotis/src/Components/Footer.jsx
+++ uzduotis/src/Components/Footer.jsx
@@ -1,7 +1,34 @@
 import React from 'react'
+import '../Styles/Footer.css';
+import InputBase from "@mui/material/InputBase"
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import { Avatar } from '@mui/material';
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+        <div className='search-bar'>
+            <div className='search'>
+                <SearchOutlinedIcon/>
+                <InputBase type="text" placeholder="Search anything"></InputBase>
+            </div>
+        </div>
+        <div className='user-side'>
+            <div className='user'>
+            <div>
+                <Avatar alt="Maxie Mon Wachid"/>
+            </div>
+            <div>
+                <p>Maxie Mon Wachid</p>
+                <div className='user-font'>
+                    <p>@Maxie Mon Wachid</p>
+                </div>
+            </div>
+        </div>
+            <div>|</div>
+            <div><NotificationsOutlinedIcon/></div>
+            </div>
+    </div>
   )
 }
